80373ba5b2f3987e6890bc7d3914c884682a82d8
Alexis Recalde <alexis.recalde@hotmail.com>
2024-01-31 21:48:46 -0300

    change id to uuid

--- src/migrations/1705551843517-create_table.ts
+++ src/migrations/1705551843517-create_table.ts
@@ -5,12 +5,13 @@ export class CreateTable1705551843517 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
+            CREATE TYPE "user_roles_enum" AS ENUM ('admin', 'user', 'guest');
             CREATE TABLE "users" (
-                "id" SERIAL NOT NULL,
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "nombre" character varying NOT NULL,
                 "apellido" character varying NOT NULL,
                 "email" character varying NOT NULL,
-                "rol" character varying NOT NULL,
+                "rol" "user_roles_enum" NOT NULL,
                 "password" character varying NOT NULL,
                 CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"),
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
